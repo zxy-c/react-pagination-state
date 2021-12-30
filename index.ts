f865e6c61f82599b4f2a7eef61b8caaad118a460
zxy <2544682494@qqcom>
2021-12-30 10:53:39 +0800

    Add SlicinationState

--- index.ts
+++ index.ts
@@ -4,86 +4,108 @@ import {
     Pageable,
     Pagination,
     paginationToPageable,
+    Slice,
+    Slicination,
+    slicinationToPageable,
 } from "@zxy-cn/pagination-model"
 import {EffectCallback, useEffect, useState} from "react"
 
-export class PaginationState {
-    private readonly paginationState
+export class SlicinationState<T extends Slicination = Slicination>{
+    protected readonly state
 
-    constructor(pagination: Pagination) {
-        this.paginationState = useState(pagination)
+    constructor(slicination: T) {
+        this.state = useState(slicination)
     }
 
-    get pagination() {
-        return this.paginationState[0]
+    get value() {
+        return this.state[0]
     }
 
-    set pagination(pagination: Pagination) {
-        this.paginationState[1](pagination)
+    set value(slicination: T) {
+        this.state[1](slicination)
     }
 
     get page() {
-        return this.pagination.page
+        return this.value.page
     }
 
     set page(page: number) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             page,
         }
     }
 
     get last() {
-        return this.pagination.last
+        return this.value.last
     }
 
     set last(last: boolean) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             last,
         }
     }
 
     get first() {
-        return this.pagination.first
+        return this.value.first
     }
 
     set first(first: boolean) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             first,
         }
     }
 
     get size() {
-        return this.pagination.size
+        return this.value.size
     }
 
     set size(size: number) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             size,
         }
     }
 
+    get pageable() {
+        return slicinationToPageable(this.value)
+    }
+
+    updateFromSlice(slice: Slice<any>) {
+        this.value = {
+            ...this.value,
+            last: slice.last,
+            first: slice.first,
+        }
+    }
+}
+
+export class PaginationState extends SlicinationState<Pagination>{
+
+    constructor(pagination: Pagination) {
+        super(pagination)
+    }
+
     get total() {
-        return this.pagination.total
+        return this.value.total
     }
 
     set total(total: number) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             total,
         }
     }
 
     get pageable() {
-        return paginationToPageable(this.pagination)
+        return paginationToPageable(this.value)
     }
 
     updateFromPage(page: Page<any>) {
-        this.pagination = {
-            ...this.pagination,
+        this.value = {
+            ...this.value,
             total: page.totalElements,
             last: page.last,
             first: page.first,
